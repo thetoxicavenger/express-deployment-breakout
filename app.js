0d859fa3dbd7621fbbb7ebc2b50a6e5eeaff78b6
Mitch Cravens <mitch.cravens@galvanize.com>
2019-05-30 15:39:15 -0700

    added env stuff

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
-// const cors = require('cors')
+const cors = require('cors')
+const compression = require('compression')
+const helmet = require('helmet')
 
 const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
-// app.use(cors())
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors())
+} else {
+    app.use(compression())
+    app.use(helmet())
+}
+
+let todos = process.env.NODE_ENV === 'development' ? require('./db/todos.json') : [{ "id": 1, "title": "Brush Teeth", "completed": true }]
 
 app.get('/api/todos', (req, res) => {
-    const todos = require('./db/todos.json')
     res.json(todos)
 })
 
 app.put('/api/todos/:id', (req, res) => {
-    let todos = require('./db/todos.json')
     todos = todos.map(todo => {
         if (todo.id === req.body.id) {
             todo = req.body
         }
         return todo
     })
-    fs.writeFileSync('./db/todos.json', JSON.stringify(todos))
+    if (process.env.NODE_ENV === 'development') {
+        fs.writeFileSync('./db/todos.json', JSON.stringify(todos))
+    }
     res.json(req.body)
 })
 
