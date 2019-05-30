cb1c13f2b411a3d927e90fc2c3cd633cff2fbc36
Mitch Cravens <mitch.cravens@galvanize.com>
2019-05-30 16:43:42 -0700

    wtf

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ const compression = require('compression')
 const helmet = require('helmet')
 require('dotenv').config()
 
-console.log(process.env.API_KEY, 'hello')
+console.log(process.env.API_KEY)
 
-const app = exress()
+const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
 if (process.env.NODE_ENV === 'development') {
     app.use(cors())
-        se {
-        app.use(compression())
-        app.use(helmet())
+} else {
+    app.use(compression())
+    app.use(helmet())
 }
 
-        todos = process.env.NODE_ENV === 'development' ? require('./db/todos.json') : [{ "id": 1, "title": "Brush Teeth", "completed": true }]
-        
-            '/api/todos', (req, res) => {
-                (todos)
-            
-            
-        put('/api/todos/:id', (req, res) => {
-        todos = todos.map(todo => {
-            if (todo.id === req.body.id) {
-                todo = req.body
+let todos = process.env.NODE_ENV === 'development' ? require('./db/todos.json') : [{ "id": 1, "title": "Brush Teeth", "completed": true }]
+
+app.get('/api/todos', (req, res) => {
+    res.json(todos)
+})
+
+app.put('/api/todos/:id', (req, res) => {
+    todos = todos.map(todo => {
+        if (todo.id === req.body.id) {
+            todo = req.body
         }
         return todo
     })
